7f1b8257974325abbfa53ae1dbbc87d1bedc8908
Fiona Burrows <fiona@myrmidonprocess.com>
2009-05-02 13:00:13 +0100

    Completely refactored user searching and editing.

--- admin/common/templates/admin.tpl.php
+++ admin/common/templates/admin.tpl.php
@@ -478,7 +478,7 @@ END;
 
 		$return .= $this -> generate_menu_entry($lang['admin_menu_new_user'], l("admin/users/add/"));
 		$return .= $this -> generate_menu_entry($lang['admin_menu_search_users'], l("admin/users/search/"));
-		$return .= $this -> generate_menu_entry("*** ".$lang['admin_menu_search_ip'], l("admin/users/ipsearch/"));
+		$return .= $this -> generate_menu_entry($lang['admin_menu_search_ip'], l("admin/users/ipsearch/"));
 		$return .= $this -> generate_menu_entry("*** ".$lang['admin_menu_profile_fields'], l("admin/profile_fields/"));
 		$return .= $this -> generate_menu_header("users", "*** ");
 
